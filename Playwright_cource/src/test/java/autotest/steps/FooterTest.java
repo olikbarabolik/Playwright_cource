883df32ad2a7fa8f48c8de4853d42bc83a54de72
User <bruk-17@yandex.ru>
2025-10-13 15:54:58 +0400

    add tests for footer and fix mistake

--- Playwright_cource/src/test/java/autotest/steps/FooterTest.java
+++ Playwright_cource/src/test/java/autotest/steps/FooterTest.java
@@ -1,7 +1,13 @@
 package autotest.steps;
 
 import autotest.config.PlaywrightConfig;
+import autotest.locators.Locators;
 import autotest.pages.PageObject;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FooterTest {
     private final PageObject pageObject;
@@ -10,5 +16,38 @@ public class FooterTest {
     public FooterTest(PageObject pageObject, PlaywrightConfig playwrightConfig) {
         this.pageObject = pageObject;
         this.playwrightConfig = playwrightConfig;
+    }
+
+    @Given("I open the Playwright footer page")
+    public void iOpenThePlaywrightTestPage() {
+        pageObject.getPage().navigate(playwrightConfig.getTestPageUrl());
+    }
+
+    @When("I check for the footer")
+    public void iCheckForTheFooter() {
+        boolean footerExists = pageObject
+                .getPage().locator(Locators.FOOTER_SELECTOR)
+                .count() > 0;
+
+        assertTrue(footerExists, "Футер должен присутствовать на странице");
+
+        boolean contactInfoLink = pageObject
+                .getPage().locator(Locators.CONTACT_LINK_SELECTOR)
+                .count() > 0;
+
+        boolean privatePolicyLink = pageObject
+                .getPage().locator(Locators.PRIVACY_POLICY_LINK_SELECTOR)
+                .count() > 0;
+        assertTrue(contactInfoLink, "Ссылка \"Контактная информация\" должна присутсвовать на странице");
+        assertTrue(privatePolicyLink, "Ссылка \"Политика конфиденциальности\" должна присутствовать на странице");
+    }
+
+    @Then("I verify the footer is displayed")
+    public void iVerifyTheFooterIsDisplayed() {
+        boolean footerExists = pageObject
+                .getPage().locator(Locators.FOOTER_SELECTOR)
+                .isVisible();
+
+        assertTrue(footerExists, "Футер должен быть виден на странице");
     }
 }
--- Playwright_cource/src/test/resources/autotest/test.feature
+++ Playwright_cource/src/test/resources/autotest/test.feature
@@ -4,3 +4,8 @@ Feature: Playwright Tests
     Given I open the Playwright catalog items page
     When I check the catalog items
     Then I verify catalog items are displayed
+
+  Scenario: Check footer
+    Given I open the Playwright footer page
+    When I check for the footer
+    Then I verify the footer is displayed
