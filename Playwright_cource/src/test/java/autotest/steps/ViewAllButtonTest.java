532e6cde3c13b4d7daea61a74e4547b9c534efac
User <bruk-17@yandex.ru>
2025-10-15 23:21:11 +0400

    add tests for navigate and navigate

--- Playwright_cource/src/test/java/autotest/steps/ViewAllButtonTest.java
+++ Playwright_cource/src/test/java/autotest/steps/ViewAllButtonTest.java
@@ -2,8 +2,35 @@ package autotest.steps;
 
 import autotest.config.PlaywrightConfig;
 import autotest.pages.PageObject;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static autotest.locators.Locators.VIEW_ALL_BUTTON_SELECTOR;
+import static org.junit.Assert.assertTrue;
 
 public class ViewAllButtonTest {
+    private final PageObject pageObject;
+    private final PlaywrightConfig playwrightConfig;
+
     public ViewAllButtonTest(PageObject pageObject, PlaywrightConfig playwrightConfig) {
+        this.pageObject = pageObject;
+        this.playwrightConfig = playwrightConfig;
+    }
+
+    @Given("I open the Playwright all button page")
+    public void iOpenThePlaywrightTestPage() {
+        pageObject.getPage().navigate(playwrightConfig.getTestPageUrl());
+    }
+
+    @When("I check the View all button")
+    public void iCheckTheViewAllButton() {
+        pageObject.getPage().click(VIEW_ALL_BUTTON_SELECTOR);
+    }
+
+    @Then("I verify the View all button redirects")
+    public void iVerifyTheViewAllButtonRedirects() {
+        String currentUrl = pageObject.getPage().url();
+        assertTrue(currentUrl.contains("/vue-app/index.html#/"));
     }
 }
--- Playwright_cource/src/test/resources/autotest/test.feature
+++ Playwright_cource/src/test/resources/autotest/test.feature
@@ -1,5 +1,10 @@
 Feature: Playwright Tests
 
+  Scenario: Check View all button
+    Given I open the Playwright all button page
+    When I check the View all button
+    Then I verify the View all button redirects
+
   Scenario: Check catalog items
     Given I open the Playwright catalog items page
     When I check the catalog items
@@ -11,16 +16,18 @@ Feature: Playwright Tests
     Then I verify the footer is displayed
 
   Scenario: Check logo
-     Given I open the Playwright logo page
-     When I check for the logo
-     Then I verify the logo is displayed
+    Given I open the Playwright logo page
+    When I check for the logo
+    Then I verify the logo is displayed
 
   Scenario: Check navigation menu
-     Given I open the Playwright pagination page
-     When I check for the pagination number
-     Then I verify the pagination is displayed
+    Given I open the Playwright pagination page
+    When I check for the pagination number
+    Then I verify the pagination is displayed
 
   Scenario: Check page H1
     Given I open the Playwright H1 header page
     When I check for the H1 header
     Then I close the browser after checking tests
+
+
